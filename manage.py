958a7fe043f731d1dca20e37e74112d597f32f32
Andela Simulatons <abdulmumeen.olasode@andela.com>
2017-02-16 22:04:30 +0100

    add changes to procfile

--- manage.py
+++ manage.py
@@ -12,14 +12,9 @@ from tasks import (
 if __name__ == "__main__":
 
     # If user passed the settings flag ignore the default wger settings
-    #if not any('--settings' in s for s in sys.argv):
-     #   setup_django_environment(get_user_config_path('wger', 'settings.py'))
     if not any('--settings' in s for s in sys.argv):
-        config_path = get_user_config_path('wger', 'settings.py')
-        print(config_path)
-        if not os.path.exists(config_path):
-            config_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'wger/settings.py')
-        setup_django_environment(config_path)
+        setup_django_environment(get_user_config_path('wger', 'settings.py'))
+
     # Alternative to above
     #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
